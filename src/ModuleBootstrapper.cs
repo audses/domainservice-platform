4a295d10602e36d58d737f20c81b233f9065cac3
Audrius <audrss@gmail.com>
2026-08-11 23:02:10 -0400

    Sync generated changes

---
+++ src/ModuleBootstrapper.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Workflowgroup.Platform;
+public static partial class ModuleBootstrapper
+{
+    /// <summary>Registers every tenant workflow's services. Add hand-wired infrastructure (crypto/hash providers, pepper descriptors, ...) here alongside the generated registrations.</summary>
+    public static IServiceCollection AddPlatformWorkflows(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddPlatformWorkflowsGenerated();
+        return serviceCollection;
+    }
+}
